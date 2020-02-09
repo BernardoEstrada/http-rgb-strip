186b30f5d256fddcb7c13fc5bf518975d4b81354
Bernardo Estrada <estradaf.bernardo@gmail.com>
2020-02-09 06:58:08 -0600

    miss

--- rgb.js
+++ rgb.js
@@ -11,10 +11,8 @@ const readline = require('readline').createInterface({
 var red = 255, green = 255, blue = 255, dr=5, dg=5, db=5;
 var color;
 
-ws281x.configure({leds:1, gpio:12});
+ws281x.configure({leds:150, gpio:12});
 
-console.log(ws281x);
-   
 
 function loop() {
 	var pixels = new Uint32Array(ws281x.leds);
@@ -33,14 +31,10 @@ function loop() {
       db*=-1;
     }
     blue+=db;
-    
+    */
     var color = (red << 16) | (green << 8)| blue;
     console.log('0x'+color.toString(16));
-*/
-readline.question(`What's your name?`, (c) => {
-  color=c;
-  readline.close()
-})
+
     for (var i = 0; i < ws281x.leds; i++){
       pixels[i] = '0x'+color;
     }
@@ -49,7 +43,7 @@ readline.question(`What's your name?`, (c) => {
     ws281x.render(pixels);
   }
 
-function run() 
+function run()
 {  // Loop every 100 ms
   //setInterval(function(){loop()}, 100);
 	loop();
