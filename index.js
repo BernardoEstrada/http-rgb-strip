5c337feb3b98aa8e98c0db0762fe7a7c6dce11ca
Bernardo Estrada <estradaf.bernardo@gmail.com>
2020-02-09 02:33:00 -0600

    Deleted unused files

--- index.js
+++ index.js
@@ -1,65 +1,99 @@
+var Color = require('color');
 const express = require('express');
 const app = express();
 app.listen(8000, () => console.log("Listening at 8000"));
 app.use(express.json());
-console.log("qwrg");
 
 let strip = {
   power: 0,
   brightness: 0,
   color: "000000",
-  cycle: false,
-  breathing: false,
-  rainbow: false
-}
+  cycle: 0,
+  breathing: 0,
+  rainbow: 0
+};
 
 function isEmptyObject(obj) {
   return !Object.keys(obj).length;
-}
+};
 
-console.log(strip.power);
-
-app.post('/on', (request, response) => {
-  strip.power = 1;
+app.get('/q/:val', (request, response) => {
+  if(request.params.val == "on"){
+    strip.power = 1;
+  } else if(request.params.val == "off"){
+    strip.power = 0;
+  }
   response.status(200).send(strip.power.toString());
 });
 
-app.post('/off', (request, response) => {
-  strip.power = 0;
-  response.status(200).send(strip.power.toString());
-});
+app.get('/brightness/:bri', (request, response) => {
+  if(!isEmptyObject(request.params.bri)){
 
-app.post('/status', (request, response) => {
-  response.status(200).send(strip.power.toString());
-});
-
-app.post('/brightness', (request, response) => {
-  console.log(request);
-  if(!isEmptyObject(request.query)){
-    if(isNaN(request.query.brightness) || request.query.brightness>100 || request.query.brightness<0){
+    if(isNaN(request.params.bri) || request.params.bri>100 || request.params.bri<0){
       response.status(400).send();
     } else{
-      strip.brightness = parseInt(request.query.brightness);
+      strip.brightness = parseInt(request.params.bri);
       response.status(200).send(strip.brightness.toString());
     }
   } else{
     response.status(200).send(strip.brightness.toString());
   }
 });
+app.get('/brightness', (request, response) => {
+  response.status(200).send(strip.brightness.toString());
+});
 
-app.post('/set', (request, response) => {
-  console.log(request);
-  if(!isEmptyObject(request.query)){
-    if(isNaN('0x' + request.query.color) || parseInt(request.query.color, 16)>parseInt('0xFFFFFF', 16)){
+app.get('/set/:col', (request, response) => {
+  if(!isEmptyObject(request.params.col)){
+    if(isNaN('0x' + request.params.col) || parseInt(request.params.col, 16)>parseInt('0xFFFFFF', 16)){
       response.status(400).send();
     } else{
-      strip.color = request.query.color;
+      strip.color = request.params.col;
+      strip.brightness = Color('#' + request.params.col).hsv().array()[2];
       response.status(200).send(strip.color);
     }
   } else{
     response.status(200).send(strip.color);
   }
 });
+app.get('/set', (request, response) => {
+  response.status(200).send(strip.color);
+});
+
+
+app.get('/cycle/:val', (request, response) => {
+  if(request.params.val == "on"){
+    strip.cycle = 1;
+    strip.breathing = 0;
+    strip.rainbow = 0;
+  } else if(request.params.val == "off"){
+    strip.cycle = 0;
+  }
+  response.status(200).send(strip.cycle.toString());
+});
+
+app.get('/breathing/:val', (request, response) => {
+  if(request.params.val == "on"){
+    strip.cycle = 0;
+    strip.breathing = 1;
+    strip.rainbow = 0;
+  } else if(request.params.val == "off"){
+    strip.breathing = 0;
+  }
+  response.status(200).send(strip.breathing.toString());
+});
+
+app.get('/rainbow/:val', (request, response) => {
+  if(request.params.val == "on"){
+    strip.cycle = 0;
+    strip.breathing = 0;
+    strip.rainbow = 1;
+  } else if(request.params.val == "off"){
+    strip.rainbow = 0;
+  }
+  response.status(200).send(strip.rainbow.toString());
+});
+
 
 /*
 "accessories": [
